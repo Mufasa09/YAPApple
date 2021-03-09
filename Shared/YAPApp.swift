9eff05d88dfa756db0a8251f541339146d797a9c
Joel Musita <hmusita2009@gmail.com>
2021-03-08 22:38:27 -0600

    Initial Commit

---
+++ Shared/YAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YAPApp.swift
+//  Shared
+//
+//  Created by Joel Musita on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct YAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
